fcfd17d19fc4b4c46d077c96915c6c1323f8c972
Serhii Hadzhilov <gadzhilov.sergey@gmail.com>
2023-06-05 23:37:32 +0300

    Fixed #14 Implement login functionality

--- src/server/routes/main.ts
+++ src/server/routes/main.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from "express";
+import { verfifyJwt } from "../middleware/jwt";
+import { AuthRequest } from "../requests/auth";
 
 export const main = express.Router();
 
-main.get("/", (req: Request, response: Response) => {
-  response.render("index");
+main.get("/", [verfifyJwt], (req: Request, response: Response) => {
+  const request = req as AuthRequest;
+  response.render("index", { user_id: request.user_id });
 });
